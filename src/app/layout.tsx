216934ef152f089d88804a923a49f5e5090b6176
Mamisoa Andriantafika <mamisoa@gmail.com>
2024-11-14 02:17:46 +0100

    no follow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,8 +46,8 @@ export const metadata: Metadata = {
 		images: [`${env.appUrl}/og-image.png`],
 	},
 	robots: {
-		index: true,
-		follow: true,
+		index: false,
+		follow: false,
 		googleBot: {
 			index: true,
 			follow: true,
